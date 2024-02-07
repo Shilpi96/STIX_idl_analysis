da5903fd310786d2c1b77f69cef8327f597adab3
Shilpi Bhunia <80779369+Shilpi96@users.noreply.github.com>
2024-02-07 14:33:12 +0000

    Create Ospex_analysfit_result_anlysis.py

---
+++ Ospex_analysfit_result_anlysis.py
@@ -0,0 +1,163 @@
+Last login: Mon Jan 29 09:18:32 on ttys001
+(base) shilpibhunia@Shilpis-MacBook-Pro ~ % bash
+
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+bash-3.2$ ariadne
+Linux ariadne 6.1.0-17-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.69-1 (2023-12-30) x86_64
+
+----------------------------------------------------------------
+
+   Welcome to DIAS ariadne.dias.ie
+
+----------------------------------------------------------------
+
+Last login: Mon Jan 29 09:18:47 2024 from 2001:770:60:ce:5061:2c86:cfa4:3b4c
+shilpi@ariadne:~$ bash
+shilpi@ariadne:~$ aodh
+(shilpi@aodh) Password: 
+Welcome to Ubuntu 20.04.6 LTS (GNU/Linux 5.4.0-169-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+  System information as of Wed  7 Feb 14:30:00 GMT 2024
+
+  System load:              0.03
+  Usage of /:               38.6% of 146.59GB
+  Memory usage:             3%
+  Swap usage:               0%
+  Temperature:              49.0 C
+  Processes:                1338
+  Users logged in:          0
+  IPv4 address for docker0: 172.17.0.1
+  IPv4 address for eno1:    160.6.202.20
+  IPv6 address for eno1:    2001:770:60:ca::20
+
+ * Strictly confined Kubernetes makes edge and IoT secure. Learn how MicroK8s
+   just raised the bar for easy, resilient and secure K8s cluster deployment.
+
+   https://ubuntu.com/engage/secure-kubernetes-at-the-edge
+
+Expanded Security Maintenance for Applications is not enabled.
+
+79 updates can be applied immediately.
+To see these additional updates run: apt list --upgradable
+
+78 additional security updates can be applied with ESM Apps.
+Learn more about enabling ESM Apps service at https://ubuntu.com/esm
+
+New release '22.04.3 LTS' available.
+Run 'do-release-upgrade' to upgrade to it.
+
+
+15 updates could not be installed automatically. For more details,
+see /var/log/unattended-upgrades/unattended-upgrades.log
+
+*** System restart required ***
+
+----------------------------------------------------------------
+
+
+	Welcome to DIAS aodh.ap.dias.ie
+
+
+   Any question/problem, please contact support@ap.dias.ie
+
+
+	Happy crunching!
+
+
+----------------------------------------------------------------
+
+-- Current Status ---------   5  15 [ Cores ] [ Users ]
+           anode.ap.dias.ie  36  36 [    64 ] [ (36 dlee)   ]
+           bnode.ap.dias.ie   _   _ [    64 ] [  ]
+           cnode.ap.dias.ie  23  21 [    64 ] [ (1 visitor) +  ]
+           dnode.ap.dias.ie   _   _ [    64 ] [  ]
+           lofar.ap.dias.ie   _   _ [    48 ] [ (1 ollama)   ]
+            aodh.ap.dias.ie   _   _ [    80 ] [  ]
+           mimir.ap.dias.ie 106 109 [   160 ] [ (9 jmackey) (1 visitor) +  ]
+             phi.ap.dias.ie   _   _ [    24 ] [  ]
+
+----------------------------------------------------------------
+
+Last login: Mon Jan 29 09:16:55 2024 from 2001:770:60:1::45
+shilpi@aodh:~$ bash
+shilpi@aodh:~$ ls
+ Aladin.jar						     ssw_idl.1293272   ssw_idl.3547424	 ssw_idl.769
+'~.bashrc'						     ssw_idl.24756     ssw_idl.3554150	 ssw_idl.7742
+ collab_projects					     ssw_idl.267570    ssw_idl.3574772	 ssw_idl.9043
+ creating_schedule_txt					     ssw_idl.2707239   ssw_idl.3577729	 ssw_idl.913863
+ env							     ssw_idl.2707684   ssw_idl.36019	 ssw_idl.942621
+ fpe0032037kHz_FE0035241anis0.30eps0.100asym1.00fr1.10.sav   ssw_idl.2714743   ssw_idl.3831297	 ssw_idl.942744
+ get-pip.py						     ssw_idl.2717863   ssw_idl.3832528	 ssw_idl.943803
+ HMI.20221111_110025.magnetogram.fits			     ssw_idl.2718357   ssw_idl.3836287	 ssw_install230518_031045.csh
+ id_rsa.pub						     ssw_idl.302358    ssw_idl.394671	 ssw_install230519_032026.csh
+ LEA_20140927_2211_srs.sav				     ssw_idl.30259     ssw_idl.4108536	 ssw_install230519_040834.csh
+ LOFAR-Sun-tools					     ssw_idl.3045      ssw_idl.4109887	 stix_eui_flarelist
+ my_script.pro						     ssw_idl.3135099   ssw_idl.4152881	 stix_idl_files
+ public_html						     ssw_idl.3135223   ssw_idl.4159208	 stx_spectrum_2202080003.fits
+ ray_new.pro						     ssw_idl.3135638   ssw_idl.4188	 stx_spectrum_2202080013.fits
+ scattering_school					     ssw_idl.3186937   ssw_idl.42187	 stx_spectrum_2211119641.fits
+ set,							     ssw_idl.3204044   ssw_idl.4352	 stx_srm_2202080003.fits
+ software_packages					     ssw_idl.3209568   ssw_idl.46639	 stx_srm_2202080013.fits
+ solarsoft_script.csh					     ssw_idl.32372     ssw_idl.48563	 stx_srm_2211119641.fits
+ spectra_radio_contours.png				     ssw_idl.327419    ssw_idl.622131	 sunpy
+ ssw							     ssw_idl.331542    ssw_idl.631756	 threecol
+ ssw_idl.1251542					     ssw_idl.3464106   ssw_idl.6584	 topcat-full.jar
+ ssw_idl.1254656					     ssw_idl.3469581   ssw_idl.687282
+ ssw_idl.1290805					     ssw_idl.3547178   ssw_idl.687367
+shilpi@aodh:~$ cd stix_idl_files/
+shilpi@aodh:~/stix_idl_files$ ls
+forloop_images			  ospex_results_23_aug_sh.fits	      stx_spectrum_2211112966.fits  testing2
+ospex_analysis			  ospex_results_6_sep_2023_all1.fits  stx_srm_2211112966.fits	    testing3
+ospex_results_23_aug_2023.fits	  paper_images			      testing			    test_maps
+ospex_results_23_aug_2023_h.fits  STIX_demo			      testing1			    timefile.csv
+shilpi@aodh:~/stix_idl_files$ cd ospex_analysis/
+shilpi@aodh:~/stix_idl_files/ospex_analysis$ ls
+ ospex_results_15_sep_2023.fits				        ospex_script_23_aug_2023.pro   stx_srm_2211112966.fits
+'ospex_results_15_sep_2023_nonthermal_peak(dont_remove).fits'   read_ospex_data.py	       test_ospex.pro
+ ospex_results_8_sep_2023_interval1.fits		        stx_spectrum_2211111572.fits   test_read_ospex.py
+ ospex_results_8_sep_2023_interval2.fits		        stx_spectrum_2211112966.fits
+ ospex_results_8_sep_2023_interval3.fits		        stx_srm_2211111572.fits
+shilpi@aodh:~/stix_idl_files/ospex_analysis$ gedit read_ospex_data.py &
+[1] 1635808
+shilpi@aodh:~/stix_idl_files/ospex_analysis$ vim read_ospex_data.py 
+
+#pdb.set_trace()
+
+##### all the info regarding params for vth+thick2 stored in hdu1[1].data['PARAMS']
+
+params = hdu1[1].data['PARAMS']
+time = hdu1[1].data['time']
+timedel = hdu1[1].data['TIMEDEL']
+sigmas = hdu1[1].data['SIGMAS']
+
+delta = [params[i][4] for i in range(params.shape[0])]
+E_c = [params[i][7] for i in range(params.shape[0])]
+d_er = [sigmas[i][4]/2 for i in range(params.shape[0])]
+E_c_er = [sigmas[i][7]/2 for i in range(params.shape[0])]
+chi_sq = [ hdu1[1].data['CHISQ'][i] for i in range(params.shape[0])]
+
+
+dt = [datetime.datetime(2022,11,11,0,0,0)+datetime.timedelta(seconds = time[i]) for i in range(params.shape[0])]
+pdb.set_trace()
+dt1 = [datetime.datetime(2022,11,11,0,0,0)+datetime.timedelta(seconds = time[i]-timedel[i]/2) for i in range(params.shape[0])]
+# we have to get the end time of the last time interval
+dt1.append(datetime.datetime(2022,11,11,0,0,0)+datetime.timedelta(seconds = time[params.shape[0]-1]+timedel[params.shape[0]-1]/2))
+#pdb.set_trace()
+fig, axs = plt.subplots(4)
+
+##### Plot delta vs time
+delta.append(delta[len(delta)-1])  ##### you have to add the last number twice for the step function
+
+axs[1].step(dt1,delta, where='post')
+pdb.set_trace()
+axs[1].errorbar(dt,delta[0:len(delta)-1], yerr = d_er, fmt = 'o')
+axs[1].set_ylabel(r'$\delta$')
+#plt.xlabel('time')
+
+                                                                                                                         66,1          50%
